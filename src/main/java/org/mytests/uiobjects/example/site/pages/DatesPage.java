e4e07b894b56bcc40dc668c6dc97173dabdf8d6c
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2023-01-04 22:23:30 -0500

    Update JDI version and create Gradle script

--- src/main/java/org/mytests/uiobjects/example/site/pages/DatesPage.java
+++ src/main/java/org/mytests/uiobjects/example/site/pages/DatesPage.java
@@ -4,6 +4,7 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.Title;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 
-@Url("/index.html") @Title("Home Page")
-public class HomePage extends WebPage {
+@Url("/dates.html") @Title("Dates")
+public class DatesPage extends WebPage {
+
 }
--- src/main/java/org/mytests/uiobjects/example/site/pages/Html5Page.java
+++ src/main/java/org/mytests/uiobjects/example/site/pages/Html5Page.java
@@ -4,6 +4,7 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.Title;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 
-@Url("/index.html") @Title("Home Page")
-public class HomePage extends WebPage {
+@Url("/html5.html") @Title("HTML 5")
+public class Html5Page extends WebPage {
+
 }
